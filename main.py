5fec7dbe9d6ec7aaacf92dfa9457aee82df4b950
dan <petrazhitskiy2000@gmail.com>
2024-03-23 13:55:31 +0300

    Прокт по Gmsh

---
+++ main.py
@@ -0,0 +1,63 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+import time
+import math
+from datetime import time
+import plotly.express as px
+import datetime
+from st_pages import Page, show_pages, add_page_title
+st.sidebar.title("MATPLOTLIb")
+
+
+
+
+add_page_title ()
+
+show_pages(
+    [
+        Page("main.py", "Введение", "🏠"),
+        Page("1.py", "Общая информация", ":books:"),
+        Page("2.py", "Одномерные графики", "📊"),
+        Page("3.py", "Элементы оформления", "📋"),
+        Page("4.py", "Чтение расчетный данных", "📝"),
+        Page("5.py", "Двумерные графики", "📈"),
+        Page("6.py", "3D Графики", "📉"),
+        Page("7.py", "Комбинирование графиков", "🖇️"),
+        Page("8.py", "Сложные визуализации", "⚙️"),
+
+    ]
+)
+
+
+
+st.title("Добро пожаловать в библиотеку Python MatPlotLib")
+
+st.subheader("В данной презентации мы раскажем о возможностях данной библиотеки")
+
+st.write("\n")
+st.write("\n")
+st.write("\n")
+st.write("\n")
+st.write("\n")
+st.write("\n")
+st.write("\n")
+st.write("\n")
+st.write("\n")
+st.write("\n")
+st.write("\n")
+st.write("\n")
+st.write("\n")
+st.write("\n")
+st.write("\n")
+st.write("\n")
+st.write("\n")
+st.write("\n")
+st.write("\n")
+
+
+
+st.subheader("Работу выполнили студенты МГУ Саров из группы СКТ-123: ")
+st.subheader("\nЮлия Вотякова, Юлия Вячеславлева, Михаил Веселов, Илья Гладченко ")
