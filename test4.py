13c347036bc2be98b432fd244019060f6116b694
dan <petrazhitskiy2000@gmail.com>
2024-03-29 18:45:58 +0300

    gmsh

--- test4.py
+++ test4.py
@@ -1,6 +1,7 @@
 import gmsh
 import meshio
 import matplotlib.pyplot as plt
+import pyvista as pv
 from mpl_toolkits.mplot3d import Axes3D
 # Инициализация Gmsh
 gmsh.initialize()
@@ -40,8 +41,20 @@ mesh = meshio.read('gmsh_test4.msh')
 # Отображение данных с помощью matplotlib
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-ax.plot_trisurf(mesh.points[:, 0], mesh.points[:, 1], mesh.points[:, 2], triangles=mesh.cells[0].data)
+ax.plot_trisurf(mesh.points[:, 0], mesh.points[:, 1], mesh.points[:, 2], triangles=mesh.cells[1].data)
 plt.show()
+
+
+
+# Загрузка файла .msh в pyvista
+mesh = pv.read('gmsh_test4.msh')
+
+# Визуализация с помощью pyvista
+plotter = pv.Plotter()
+plotter.add_mesh(mesh)
+plotter.show()
+
+
 # It finalize the Gmsh API
 gmsh.finalize()
 
